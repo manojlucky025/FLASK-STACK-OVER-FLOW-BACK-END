45fa1339fd7228044abab22480a0a815bee33c6a
Manoj S H <msatha@innominds.com>
2021-11-28 15:54:19 +0530

    login page

---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import render_template, flash, redirect, url_for, session, g, request, Blueprint
+from flask_login import login_required, login_user, logout_user, current_user
+
+mod_auth = Blueprint('mod_auth', __name__)
+
+@mod_auth.route('/register', methods=['GET', 'POST'])
+def register():
+	form = RegistrationForm()
+	if form.validate_on_submit():
+		user = User(email = form.email.data, username = form.username.data, password = form.password.data)
+		db.session.add(user)
+		db.session.commit()
+		flash("You have succesfully registered! You may now login")
+		return redirect(url_for('mod_auth.login'))
+	return render_template('mod_auth/register.html',form = form, title = "Register")
+
+@mod_auth.route('/login', methods = ['GET', 'POST'])
+def login():
+	form = LoginForm()
+	if form.validate_on_submit():
+		user = User.query.filter_by(email = form.email.data).first()
+		if user is not None and user.verify_password(form.password.data):
+			login_user(user)
+			return redirect(url_for('mod_user.dashboard'))
+		else:
+			flash('Invalid email or password.')
+	return render_template('mod_auth/login.html', form = form, title = 'Login')
+
+@mod_auth.route('/logout', methods = ['GET', 'POST'])
+def logout():
+	logout_user()
+	flash('You have been succesfully logged out.')
+	return redirect(url_for('mod_auth.login'))
+
+
+
+
+
